1295e7cd44b1794cf87937cc624fcdd63870c60a
Renat Yakublevich <49581209+RenatYakublevich@users.noreply.github.com>
2020-11-17 20:13:31 +0300

    alpha 1.3
    add mood feed

--- database.py
+++ database.py
@@ -6,6 +6,7 @@ class Database:
         self.connection = sqlite3.connect(database_file)
         self.cursor = self.connection.cursor()
 
+
     def add_user(self, name, telegram_username):
         """
         Функция добавляет пользователя в таблицу users
@@ -21,6 +22,7 @@ class Database:
         except sqlite3.IntegrityError:
             pass
 
+
     def show_info_user(self, info_param, telegram_username):
         """
         :param info_param: параметр для возврата функцией
@@ -30,6 +32,17 @@ class Database:
         with self.connection:
             return self.cursor.execute(f"SELECT {info_param} FROM `users` WHERE `telegram_username` = ?",
                                        (telegram_username,)).fetchone()[0]
+
+
+    def update_info_user(self, info_param, info_param_value, telegram_username):
+        """
+        :param info_param: параметр для обновления
+        :param info_param_value: значения параметра для обновления
+        :param telegram_username: уникальный никнейм в телеграм
+        :return: None
+        """
+        with self.connection:
+            self.cursor.execute(f'UPDATE `users` SET `last_view_mood` = ? WHERE `telegram_username` = ?',(info_param_value,telegram_username))
 
     def add_mood(self, text, telegram_username, type):
         """
@@ -45,20 +58,34 @@ class Database:
             self.cursor.execute('UPDATE `users` SET `count_moods` = ? WHERE `telegram_username` = ?',
                                        (int(self.show_info_user('count_moods', telegram_username)) + 1, telegram_username))
 
-            # прибавляем +1 к количеству поинтов
+            # прибавляем +5 к количеству поинтов
             self.cursor.execute('UPDATE `users` SET `points` = ? WHERE `telegram_username` = ?',
                                 (int(self.show_info_user('points', telegram_username)) + 5, telegram_username))
             return self.cursor.execute(
                 "INSERT INTO `moods` (`text`, `telegram_username`, `type`) VALUES(?,?,?)",
                 (text, telegram_username, type_bool))
 
-    def show_info_mood(self):
+
+    def show_info_mood(self, mood_id):
+        """
+        :param mood_id: айди записи
+        :return: функция возвращает информацию о записе по её айди
+        """
         with self.connection:
-            pass
+            return self.cursor.execute(f"SELECT `type`,`telegram_username`,`text` FROM `moods` WHERE `id` = ?",
+                                       (mood_id,)).fetchone()
+
 
     def show_rating(self):
+        """
+        :return: функция возвращает топ 5 пользователей по очкам
+        """
         with self.connection:
-            with self.connection:
-                return self.cursor.execute(
-                    'SELECT `telegram_username` FROM `users` ORDER BY `points` DESC LIMIT 5').fetchall()
+            return self.cursor.execute(
+                'SELECT `telegram_username` FROM `users` ORDER BY `points` DESC LIMIT 5').fetchall()
+
+
+# db = Database('db_model.db') # FOR DEBUG
+
+# print(db.update_info_user(info_param='x',info_param_value=2,telegram_username='dop3file')) # FOR DEBUG
 
--- server.py
+++ server.py
@@ -2,13 +2,11 @@ import logging
 
 from aiogram import Bot, types
 from aiogram.utils import executor
-from aiogram.utils.emoji import emojize
 from aiogram.dispatcher import Dispatcher
-from aiogram.types.message import ContentType
-from aiogram.utils.markdown import text, bold, italic, code, pre
 from aiogram.types import ParseMode, InputMediaPhoto, InputMediaVideo, ChatActions
 from aiogram.types import ReplyKeyboardRemove,ReplyKeyboardMarkup, KeyboardButton, \
                           InlineKeyboardMarkup, InlineKeyboardButton
+
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
@@ -17,7 +15,18 @@ import config
 from database import Database
 
 
-logging.basicConfig(level=logging.INFO)
+# логирование
+logging.basicConfig(filename="all_log.log", level=logging.INFO, format='%(asctime)s - %(levelname)s -%(message)s')
+warning_log = logging.getLogger("warning_log")
+warning_log.setLevel(logging.WARNING)
+
+fh = logging.FileHandler("warning_log.log")
+
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+
+
+warning_log.addHandler(fh)
 
 API_TOKEN = config.TOKEN
 
@@ -29,28 +38,38 @@ db = Database('db_model.db')
 
 @dp.message_handler(commands=['start', 'help'], state='*')
 async def start(message: types.Message):
-    button_profile = KeyboardButton('Профиль👤')
-    button_add_mood = KeyboardButton('Добавить муд📝')
-    button_rating = KeyboardButton('Рейтинг🏆')
+    try:
+        button_profile = KeyboardButton('Профиль👤') # Done!
+        button_add_mood = KeyboardButton('Добавить муд📝') # Done!
+        button_rating = KeyboardButton('Рейтинг🏆') # Done!
+        button_feed = KeyboardButton('Лента📰') # 50 / 50 фикс баг с тем что повторяется последний муд
 
-    menu = ReplyKeyboardMarkup()
-    menu.add(button_add_mood, button_profile, button_rating)
+        menu = ReplyKeyboardMarkup()
+        menu.add(button_add_mood, button_profile, button_rating, button_feed)
 
-    db.add_user(name=message.from_user.first_name, telegram_username=message.from_user.username)
-    await message.answer(f"Привет {message.from_user.first_name.title()}!👋\n\n" \
-                         f"Это телеграм бот Mood😎\nМесто, где ты можешь поделится своим мудом на сегодня\n" \
-                         f"И не важно чёрный он или белый :)", reply_markup=menu)
+        db.add_user(name=message.from_user.first_name, telegram_username=message.from_user.username)
+        await message.answer(f"Привет {message.from_user.first_name.title()}!👋\n\n" \
+                             f"Это телеграм бот Mood😎\nМесто, где ты можешь поделится своим мудом на сегодня\n" \
+                             f"И не важно чёрный он или белый :)", reply_markup=menu)
+    except Exception as e:
+        warning_log.warning(e)
+
 
 @dp.message_handler(lambda message: message.text.lower().startswith('профиль'), state='*')
 async def profile(message: types.Message):
-    await message.answer(f'Ваш ник - {db.show_info_user(info_param="name",telegram_username=message.from_user.username).title()}\n' \
-                         f'Количество очков - {db.show_info_user(info_param="points", telegram_username=message.from_user.username)}\n' \
-                         f'Количество мудов - {db.show_info_user(info_param="count_moods", telegram_username=message.from_user.username)}\n')
+    try:
+        await message.answer(f'Ваш ник - {db.show_info_user(info_param="name",telegram_username=message.from_user.username).title()}\n' \
+                             f'Количество очков - {db.show_info_user(info_param="points", telegram_username=message.from_user.username)}\n' \
+                             f'Количество мудов - {db.show_info_user(info_param="count_moods", telegram_username=message.from_user.username)}\n')
+    except Exception as e:
+        warning_log.warning(e)
+
 
 # класс для работы с машиной состояний
 class MoodParams(StatesGroup):
     type = State()
     text = State()
+
 
 @dp.message_handler(lambda message: message.text.lower().startswith('добавить муд'), state='*')
 async def add_mood(message: types.Message):
@@ -63,20 +82,26 @@ async def add_mood(message: types.Message):
     await message.answer('Напиши тип своего муда\nОн может быть белым - 🤍\nА может быть чёрным - 🖤',reply_markup=types_mood)
     await MoodParams.type.set()
 
+
 @dp.message_handler(state=MoodParams.type)
 async def input_mood_type(message: types.Message, state: FSMContext):
     await state.update_data(type=message.text)
     await message.answer('Прекрасно!\nТеперь опиши свой муд парочкой слов')
     await MoodParams.next()
 
+
 @dp.message_handler(state=MoodParams.text)
 async def input_mood_text(message: types.Message, state: FSMContext):
-    await state.update_data(text=message.text)
-    await message.answer('Отлично!\nТвой муд опубликован')
-    user_data = await state.get_data() # словарь с всеми переменными машины состояния
-    db.add_mood(text=user_data['text'], type=user_data['type'], telegram_username=message.from_user.username) # добавляем запись в дб
-    await state.finish()
-    await start(message)
+    try:
+        await state.update_data(text=message.text)
+        await message.answer('Отлично!\nТвой муд опубликован')
+        user_data = await state.get_data() # словарь с всеми переменными машины состояния
+        db.add_mood(text=user_data['text'], type=user_data['type'], telegram_username=message.from_user.username) # добавляем запись в дб
+        await state.finish()
+        await start(message)
+    except Exception as e:
+        warning_log.warning(e)
+
 
 # TODO : обернуть в декоратор 
 @dp.message_handler(commands=['exit'], state='*')
@@ -84,17 +109,40 @@ async def exit(message: types.Message, state: FSMContext):
     await state.finish()
     await start(message)
 
+
 @dp.message_handler(lambda message: message.text.lower().startswith('рейтинг'), state='*')
 async def show_rating(message: types.Message):
-    place_num = 1
-    rating = ''
-    for place in db.show_rating():
-        rating += f'{place_num} место - {db.show_info_user("name",place[0]).title()}'
-        place_num += 1
-    await message.answer(rating)
+    try:
+        place_num = 1
+        rating = ''
+        for place in db.show_rating():
+            rating += f'{place_num} место - {db.show_info_user("name",place[0]).title()}'
+            place_num += 1
+        await message.answer(rating)
+    except Exception as e:
+        warning_log.warning(e)
 
 
+@dp.message_handler(lambda message: message.text.startswith('Лента'), state='*')
+async def show_mood_feed(message: types.Message):
+    button_like = KeyboardButton('❤')
+    button_next= KeyboardButton('➡')
 
+    menu = ReplyKeyboardMarkup()
+    menu.add(button_like, button_next)
+
+    await message.answer(f'{"🖤" if db.show_info_mood(db.show_info_user("last_view_mood",message.from_user.username))[0] == "0" else "🤍"}\n{db.show_info_mood(db.show_info_user("last_view_mood",message.from_user.username))[2]}',reply_markup=menu)
+
+
+@dp.message_handler(lambda message: message.text.startswith('➡'), state='*')
+async def show_mood_feed_next(message: types.Message):
+    try:
+        await message.answer(
+            f'{"🖤" if db.show_info_mood(db.show_info_user("last_view_mood",message.from_user.username))[0] == "0" else "🤍"}\n{db.show_info_mood(db.show_info_user("last_view_mood", message.from_user.username) + 1)[2]}')
+    except TypeError:
+        db.update_info_user(info_param='last_view_mood',info_param_value=1,telegram_username=message.from_user.username)
+        await message.answer(
+            f'{"🖤" if db.show_info_mood(db.show_info_user("last_view_mood",message.from_user.username))[0] == "0" else "🤍"}\n{db.show_info_mood(db.show_info_user("last_view_mood", message.from_user.username))[2]}')
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
